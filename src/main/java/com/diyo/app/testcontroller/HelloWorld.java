fb736d6bb64198c97b08f28556266287ffc541fa
Bishwo Bhandari <bhanbish@isu.edu>
2023-06-26 09:18:23 -0400

    new changes

--- src/main/java/com/diyo/app/testcontroller/HelloWorld.java
+++ src/main/java/com/diyo/app/testcontroller/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.diyo.diyo.controller;
+package com.diyo.app.testcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
