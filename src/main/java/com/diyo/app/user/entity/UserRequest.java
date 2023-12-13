5e4afdd0187d2c333a0747a4ba65588e26afc68f
Sudip Adhikari <shudip79@gmail.com>
2023-12-13 14:12:41 -0500

    Added input validation and exception handling

--- src/main/java/com/diyo/app/user/entity/UserRequest.java
+++ src/main/java/com/diyo/app/user/entity/UserRequest.java
@@ -1,5 +1,8 @@
 package com.diyo.app.user.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,10 +13,16 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 @Builder
 public class UserRequest {
+    @NotBlank
     private String firstName;
     private String middleName;
+    @NotBlank
     private String lastName;
+    @NotBlank
     private String email;
+    @NotBlank
+    @Pattern(regexp = "^[0-9- ]+$", message = "Invalid phone number")
     private String phoneNumber;
+    @NotNull
     private MultipartFile resume;
 }
