593535f2d71f416fe742673558d1fb9c54cbd8f8
Bishwo Bhandari <bishwo@diyotech.net>
2023-12-15 18:38:46 -0500

    new changes for deployment

--- src/main/java/com/diyo/app/config/AWSConfig.java
+++ src/main/java/com/diyo/app/config/AWSConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSConfig {
-    @Value("${aws.accessKeyId}")
+    @Value("${aws.accessKey}")
     private String accessKey;
 
     @Value("${aws.secretKey}")
